7ef2ad3edc152ae9c5279d6c91cd599a3b40b174
soundestmammal <robert.checco@gmail.com>
2017-06-24 19:20:36 -0400

    This is the first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule
